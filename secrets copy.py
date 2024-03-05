29438a505882da39785325f0dbbecc273b2a7f3c
ObiEuan <geuan@modelo.edu.mx>
2024-03-04 18:33:34 -0600

    copia de secrets plantilla

---
+++ secrets copy.py
@@ -0,0 +1,9 @@
+# config.py
+ssid = '' #red wifi
+password = '' #de la red wifi
+tokenPanelPico = '' #obtenido de portal
+tokenSalon = '' #obtenido de portal
+urlPanelPico = "" #panel pico
+urlSalon = "" #panel asistencias
+retraso = 900000 # PING cada 15 minutos (900000 ms)
+
